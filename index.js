21bd7085a81e810009b1a438a033e511fdf4b74c
Tim Davidson <timdavidson2@gmail.com>
2021-11-11 02:51:40 -0500

    add index and graphql server

---
+++ index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Hello World!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB)
+  .then(() => {
+    console.log("MONGODB Connected");
+    return server.listen({ port: 3000 });
+  })
+  .then((res) => {
+    console.log(`Server running on port ${res.url}`);
+  });
