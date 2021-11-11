5baaf754cc79def89a6151ede301e4f025327894
Tim Davidson <timdavidson2@gmail.com>
2021-11-11 14:30:47 -0500

    update validators with error statements

--- util/validators.js
+++ util/validators.js
@@ -8,4 +8,22 @@ module.exports.validateRegisterInput = (
   if (username.trim() === "") {
     errrors.username = "Username cannot be empty";
   }
+  if (email.trim() === "") {
+    errrors.email = "Email cannot be empty";
+  } else {
+    const regEx =
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email is not valid format";
+    }
+    if (password === "") {
+      errors.password = "Password cannot be empty";
+    } else if (password !== confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
 };
