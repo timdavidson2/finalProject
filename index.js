43929c72fc171c57dd0d91b83e298be775e21222
Tim Davidson <timdavidson2@gmail.com>
2021-11-11 10:18:33 -0500

    add typeDefs and resolvers

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ const Post = require("./models/Post");
 const { MONGODB } = require("./config");
 
 const typeDefs = gql`
+  type Post {
+    _id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World!",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
