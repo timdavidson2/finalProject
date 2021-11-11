79356853f08b3e3fbe9c440ae83fc51c9b4a04e1
Tim Davidson <timdavidson2@gmail.com>
2021-11-11 12:54:02 -0500

    add token

--- schema/resolvers/users.js
+++ schema/resolvers/users.js
@@ -1,11 +1,35 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
-    register(_, args, context, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
       // TODO Validate user data
       // TODO not duplicate user
       // TODO hash password and create token
+      password = await bcrypt.hash(password, 10);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString,
+      });
+      const res = await newUser.save();
+
+      const token = jwt.sign({
+        _id: res.id,
+        email: res.email,
+        username: res.username,
+      });
     },
   },
 };
