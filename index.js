7d0a01aaea674161562213e605aebd85dc8f64db
Tim Davidson <timdavidson2@gmail.com>
2021-11-11 09:58:27 -0500

    add post model to index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const Post = require("./models/Post");
 const { MONGODB } = require("./config");
 
 const typeDefs = gql`
